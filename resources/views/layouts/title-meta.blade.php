52a212275f4d4204d2963765c50df97a55693d57
alfin <fachrizalalfin@gmail.com>
2024-05-16 10:43:38 +0700

    icon

--- resources/views/layouts/title-meta.blade.php
+++ resources/views/layouts/title-meta.blade.php
@@ -4,4 +4,4 @@
 <meta content="Register Outlet | Indraco Global Indonesia" name="description" />
 <meta content="Indraco Webdev" name="author" />
 <!-- App favicon -->
-<link rel="shortcut icon" href="{{ URL::asset('assets/images/ikon.png')}}">
+<link rel="shortcut icon" href="{{ URL::asset('assets/images/redtop.png')}}">
