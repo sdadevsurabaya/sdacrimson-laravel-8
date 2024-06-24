9429120590072785a6a7993b1e1df51e2e559bc0
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2024-06-24 10:47:22 +0700

    Update rekap index

--- resources/views/reportsales/index.blade.php
+++ resources/views/reportsales/index.blade.php
@@ -54,7 +54,7 @@
                             <thead>
                                 <tr>
                                     <th>NO</th>
-                                    <th>Kode</th>
+                                  
                                     <th>AR</th>
                                     <th>Tanggal</th>
                                     <th width="280px">Aksi</th>
@@ -65,7 +65,7 @@
                                 @foreach ($jadwals as $key => $jadwal)
                                     <tr>
                                         <td>{{ $loop->iteration }}</td> {{-- This will give you the sequential number --}}
-                                        <td>{{ $jadwal->kode }}</td> {{-- Assuming 'user' relation has 'name' attribute --}}
+                                    
                                         <td>{{ $jadwal->user->name }}</td> {{-- Assuming 'user' relation has 'name' attribute --}}
                                         <td>{{ \Carbon\Carbon::parse($jadwal->date)->format('d-M-Y') }}</td>
                                         {{-- Formatting the date --}}
